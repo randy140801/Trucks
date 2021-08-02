94487dc314396ef398503ec209b41c251136aa53
Randy Frias <69165606+randy140801@users.noreply.github.com>
2021-08-02 14:22:36 -0400

    Update conect.php

--- Application/conect.php
+++ Application/conect.php
@@ -2,12 +2,4 @@
 
 $connect = mysqli_connect("trucksrf.mysql.database.azure.com","randy140801@trucksrf","Randy0801","trucks");
 
-if($connect)
-{
-  echo'CONECTADO';
-}
-else
-{
-  echo'AMIGO NO ESTA CONECTADO';
-}
 ?>
